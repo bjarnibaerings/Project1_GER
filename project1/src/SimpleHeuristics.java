67bc371c2a11e761f38309ea219f69a2e748dd84
bjarnibaerings <bjarnibh@outlook.com>
2024-02-17 16:14:17 +0000

    LAga simple

--- project1/src/SimpleHeuristics.java
+++ project1/src/SimpleHeuristics.java
@@ -8,7 +8,7 @@ public class SimpleHeuristics implements Heuristics {
     public double eval(State current_state) {
         int most_advanced_white_2 = get_most_advanced_piece(current_state, WHITE);
         // distance of most advanced black piece to row 1> - <distance of most advanced white piece to row H>
-        if (most_advanced_white_2 == height-1) {
+        if (most_advanced_white_2 == env.height-1) {
             return 100;
         }
         
@@ -18,14 +18,14 @@ public class SimpleHeuristics implements Heuristics {
         }
 
         // terminal state
-       return ((most_advanced_black_2-1)-(height-most_advanced_white_2));
+       return ((most_advanced_black_2-1)-(env.height-most_advanced_white_2));
     }
 
     public int get_most_advanced_piece(State state, char friendly){
         if (friendly == 'W') {
             // start at the top (BLACK) and go down the board
-            for(int y = height-1; y > 0; y--){
-                for(int x = 0; x < env.width; x++){
+            for(int y = env.height-1; y > 0; y--){
+                for(int x = 0; x < this.env.width; x++){
                     if (state.board[y][x] == friendly ) {
                         return y;
                     }
@@ -34,7 +34,7 @@ public class SimpleHeuristics implements Heuristics {
         }
         // if black
         // start at the bottem (WHITE) and go UP the board
-        for(int y = height; y < env.height; y++){
+        for(int y = 0; y < env.height; y++){
             for(int x = 0; x < env.width; x++){
                 if (state.board[y][x] == friendly ) {
                     return y;
